dedf21ab1fde80a6be919020c40d300e5e71cf0f
OrangeWiz <gopi.rayini@gmail.com>
2021-02-11 14:41:04 -0500

    Camera movement updates

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour
 {
+    static public GameObject POI;
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+    [Header("Set Dynamically")]
+    public float camZ;
+    
+    void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        if (POI == null) return;
+
+        Vector3 destination = POI.transform.position;
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.z = camZ;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        transform.position = destination;
+
+        Camera.main.orthographicSize = destination.y + 10;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
